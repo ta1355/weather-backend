2bb4b32046ab370664d8746fddb861aa699358b9
Jack <jack@gmail.com>
2024-08-09 13:11:41 +0900

    1:11pm

--- src/main/java/Apitest/demo/WeatherRestController.java
+++ src/main/java/Apitest/demo/WeatherRestController.java
@@ -1,8 +1,7 @@
 package Apitest.demo;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -10,16 +9,17 @@ public class WeatherRestController {
 
 
 
-    @GetMapping("/Today_Time_Weather")
-    public Weather_Tool2 Data1 (){
+    @GetMapping("/Today_Time_Weather/{Date}")
+    public Weather_Tool2 Data1 (@PathVariable Long Date){
 
         RestTemplate Data1 = new RestTemplate();
         String url = "https://apihub.kma.go.kr/api/typ01/url/kma_sfctm2.php?tm=202408090900&stn=108&help=1&authKey=JbGQz-C3S5CxkM_gtwuQQw";
 
         String response1 = Data1.getForObject(url, String.class);
 
+        System.out.println(Date);
 
-       Weather new1 = DataEdit1(response1);
+        Weather new1 = DataEdit1(response1);
 
         return new Weather_Tool2(new1.getTemperature());
 
