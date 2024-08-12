7d4edc90791c1d0fcf0054c5cf74925528cb7fe9
yoo <eorbs255@naver.com>
2024-08-12 20:50:02 +0900

    Like city 추가

---
+++ src/test/java/Monitoring/Project/weather/user/SelectLikedCity.java
@@ -0,0 +1,33 @@
+package Monitoring.Project.weather.user;
+
+
+
+import Monitoring.Project.weather.weathers.KoreanCity;
+import Monitoring.Project.weather.weathers.now.NowApi;
+import Monitoring.Project.weather.weathers.now.NowResponseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SelectLikedCity {
+
+    // 메서드
+    public List<NowResponseDto> addResponseCities(User user) {
+
+        List<KoreanCity> likedList = user.getLikedList();
+        List<KoreanCity> responseCities = new ArrayList<>();
+
+        for (KoreanCity city : likedList) {
+            if(likedList.contains(city)) {
+                responseCities.add(city);
+            }
+        }
+
+        return responseCities.stream().map(
+                city -> NowApi.fetchWeather(city)
+        ).toList();
+    }
+
+}
