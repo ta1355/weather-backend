d9f895b7b67a4440f74d8aa6a9f27970c19ac56e
yoo <eorbs255@naver.com>
2024-08-12 20:50:16 +0900

    RegisterUserRequestDto 추가

---
+++ src/test/java/Monitoring/Project/weather/user/RegisterUserRequestDto.java
@@ -0,0 +1,15 @@
+package Monitoring.Project.weather.user;
+
+import java.time.LocalDate;
+
+public record RegisterUserRequestDto(
+
+        String email,
+        String password,
+        String nickname,
+        LocalDate birthday
+)
+{
+
+}
+
