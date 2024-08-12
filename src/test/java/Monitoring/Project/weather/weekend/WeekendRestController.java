5861be6ea1e9ffc187cf7dce51caac34b729e151
yoo <eorbs255@naver.com>
2024-08-12 20:23:11 +0900

    weekend Controller, Service, dto 추가

---
+++ src/test/java/Monitoring/Project/weather/weekend/WeekendRestController.java
@@ -0,0 +1,26 @@
+package Monitoring.Project.weather.weekend;
+
+
+import Monitoring.Project.weather.KoreanCity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WeekendRestController {
+
+    private final WeekendService weekendService;
+
+    public WeekendRestController(WeekendService weekendService) {
+        this.weekendService = weekendService;
+    }
+
+    @GetMapping("/weekend/{city}")
+    public List<WeekendResponseDto.WeatherData> Seoul(@PathVariable String city) {
+        KoreanCity koreanCity = KoreanCity.fromString(city);
+        return weekendService.weekend(koreanCity);
+    }
+
+}
---
+++ src/test/java/Monitoring/Project/weather/weekend/WeekendService.java
@@ -0,0 +1,27 @@
+package Monitoring.Project.weather.weekend;
+
+
+import Monitoring.Project.weather.KoreanCity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class WeekendService {
+
+    @Value("${secretKeyKim}") String secretKey;
+
+    public List<WeekendResponseDto.WeatherData> weekend(KoreanCity city) {
+        RestClient restClient = RestClient.create();
+        WeekendResponseDto body = restClient.get()
+                .uri("https://api.openweathermap.org/data/2.5/forecast?q="+ city.getName() +"&appid=" + secretKey + "&units=metric")
+                .retrieve()
+                .body(WeekendResponseDto.class);
+        return body.list().stream().filter(
+                weatherData -> weatherData.dt_txt().endsWith("12:00:00")).toList();
+
+    }
+
+}
