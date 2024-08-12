439e56fcabaef00ff2929319f2409416bcfb338c
yoo <eorbs255@naver.com>
2024-08-12 20:12:18 +0900

    city enum 추가

---
+++ src/test/java/Monitoring/Project/weather/KoreanCity.java
@@ -0,0 +1,37 @@
+package Monitoring.Project.weather;
+
+public enum KoreanCity {
+    SEOUL("Seoul"),
+    BUSAN("Busan"),
+    DAEGU("Daegu"),
+    INCHEON("Incheon"),
+    GWANGJU("Gwangju"),
+    DAEJEON("Daejeon"),
+    ULSAN("Ulsan"),
+    SEJONG("Sejong"),
+    JEJU("Jeju");
+
+    private final String name;
+
+    KoreanCity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static KoreanCity fromString(String name) {
+        for (KoreanCity city : KoreanCity.values()) {
+            if (city.name.equalsIgnoreCase(name)) {
+                return city;
+            }
+        }
+        throw new IllegalArgumentException("Unknown city: " + name);
+    }
+}
